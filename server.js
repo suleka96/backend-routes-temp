72f4c429ecde180455735484fa7f45cfc90a7c1f
Raneesh Gomez <raneesh2307@gmail.com>
2018-03-28 11:47:57 +0530

    updated requests schema

--- server.js
+++ server.js
@@ -513,7 +513,7 @@ User.findOne({ "userId": "aaaaaaaaaa" }, { "requests": 1, "_id": 0 }, function (
           array.push({userId: record.userId ,fName: record.fName, lName: record.lName, bio: record.bio });  
           console.log("resultttttttttttt"+JSON.stringify(array));               
       });
-      if( i+1 == parsedObj.requests.length){
+      if( i == (parsedObj.requests.length-1)){
         console.log("result babes"+JSON.stringify(array));
       }    
     }   
