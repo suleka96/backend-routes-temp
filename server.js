3c97a7a00cd48dc5463dde1413cfb1152ef2a224
Raneesh Gomez <raneesh2307@gmail.com>
2018-03-28 14:16:31 +0530

    updated requests schema

--- server.js
+++ server.js
@@ -472,13 +472,14 @@ app.post("/device/requests/return", function (req, res) {
     let array = [];//store JS object
 
     for (var i = 0; i < parsedObj.requests.length; i++) {      
-      console.log("JS value " + i + ": " + parsedObj.requests[i].requesterId);  
+      console.log("JS value " + i + ": " + parsedObj.requests[i].requesterId);
+      console.log("result babes"+JSON.stringify(array));       
       User.findOne({ userId: parsedObj.requests[i].requesterId }).then(function (record) {
           console.log("profile retrieved successfully");
           array.push({userId: record.userId ,fName: record.fName, lName: record.lName, bio: record.bio });  
           console.log("resultttttttttttt"+JSON.stringify(array));     
       });
-        console.log("result babes"+JSON.stringify(array));     
+        
         res.json(array); 
     }
     
