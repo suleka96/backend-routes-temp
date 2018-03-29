40be965bd0a1dbdb8c082956cddf6ec0c05549ac
Raneesh Gomez <raneesh2307@gmail.com>
2018-03-29 14:06:26 +0530

    updated requests schema

--- server.js
+++ server.js
@@ -578,13 +578,14 @@ User.findOne({ "userId": "aaaaaaaaaa" }, { "receivedProfiles": 1, "_id": 0 }).th
 
         User.findOne({ "profiles._profileId": parsedObj.receivedProfiles[i].receivedProfileId[j] }, { "profiles": 1, "_id": 0 }).then(function(profile) {
         console.log(profile);
+        console.log(profile.facebookURL);
         array.push({
-            _profileId: profile.profiles._profileId,
-            profileName: profile.profiles.profileName,
-            mobileNo: profile.profiles.mobileNo,
-            dateOfBirth: profile.profiles.dateOfBirth,
-            homeAddress: profile.profiles.homeAddress,
-            email: profile.profiles.email
+            _profileId: profile._profileId,
+            profileName: profile.profileName,
+            mobileNo: profile.mobileNo,
+            dateOfBirth: profile.dateOfBirth,
+            homeAddress: profile.homeAddress,
+            email: profile.email
             // links: {
             //   facebookURL: profile.links.facebookURL,
             //   twitterURL: profile.links.twitterURL,
