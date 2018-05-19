905c154c16eab310f5316fa7391811ea1430ff69
suleka96 <sulekahelmini96comp@gmail.com>
2018-05-19 11:11:28 +0530

    testing dilu's route

--- server.js
+++ server.js
@@ -1509,7 +1509,7 @@ acquiring data and sending them to the client
 
       User.update(
         { userId: received.Device_ID },
-        { $set: { requests: { element } } },
+        { $set: { "requests.$":  element  } },
         { safe: true },
         function(err, obj) {
           if (err) {
